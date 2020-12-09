ad966d3fb3915f8dd10ff6663230abe5fe8cd3cb
Ramon-Goveia <ramonalangoveia@hotmail.com>
2020-12-09 14:59:45 -0300

    Craindo Navbar e Container Jumbotron

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,51 @@
+import React, { useState } from 'react'
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    Container,
+    Jumbotron,
+    Button
+} from 'reactstrap'
+
 function HomePage() {
-    return <div>Welcome to Next.js!</div>
-  }
-  
-  export default HomePage
+    const [isOpen, setIsOpen] = useState(false)
+
+    const toggle = () => setIsOpen(!isOpen)
+
+    return (
+        <div>
+            <Navbar color="dark" dark expand="md">
+                <Container>
+                    <NavbarBrand href="/">Rhayp</NavbarBrand>
+                    <NavbarToggler onClick={toggle} />
+                    <Collapse isOpen={isOpen} navbar>
+                        <Nav className="mr-auto" navbar>
+                            <NavItem>
+                                <NavLink href="/">Orçamento</NavLink>
+                            </NavItem>
+                        </Nav>
+                    </Collapse>
+                </Container>
+            </Navbar>
+
+            <Jumbotron>
+                <Container>
+                    <h1 className="display-3">Hello, world!</h1>
+                    <p className="lead">This is a simple hero unit, a simple Jumbotron-style component for calling extra attention to featured content or information.</p>
+                    <hr className="my-2" />
+                    <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+                    <p className="lead">
+                        <Button color="primary">Learn More</Button>
+                    </p>
+                </Container>
+            </Jumbotron>
+        </div>
+    )
+}
+
+export default HomePage
